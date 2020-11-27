849d55315853a658f5bdff8bf0694c72d2751ffa
Zhang-dev <55280516+Zhang-dev@users.noreply.github.com>
2020-11-27 15:30:20 +0100

    Refactor sturcture of modules

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -14,10 +14,10 @@ export class AppComponent {
   ngOnInit() {
     console.log(window.innerWidth)
     if (window.innerWidth < 768) {
-      this.sidenav.fixedTopGap = 55;
+      //this.sidenav.fixedTopGap = 55;
       this.opened = false;
     } else {
-      this.sidenav.fixedTopGap = 55;
+      //this.sidenav.fixedTopGap = 55;
       this.opened = true;
     }
   }
@@ -25,17 +25,17 @@ export class AppComponent {
   @HostListener('window:resize', ['$event'])
   onResize(event) {
     if (event.target.innerWidth < 768) {
-      this.sidenav.fixedTopGap = 55;
+     // this.sidenav.fixedTopGap = 55;
       this.opened = false;
     } else {
-      this.sidenav.fixedTopGap = 55
+      //this.sidenav.fixedTopGap = 55
       this.opened = true;
     }
   }
 
   isBiggerScreen() {
     const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    if (width < 768) {
+    if (width > 768) {
       return true;
     } else {
       return false;
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { AngularMaterialModule } from './material.module';
-import { AppRoutingModule } from './app-routing.module';
+import { AngularMaterialModule } from './material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { AppRoutingModule } from './routing/routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeaderComponent,
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent],
